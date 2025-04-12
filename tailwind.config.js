d28341da490b6797a9e37277fb1376ccc5be09aa
yakatyansh <ykpetrolhead08@outlook.com>
2025-04-12 12:39:42 +0530

    we roll

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './app/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {
+        animation: {
+          wiggle: 'wiggle 1s ease-in-out infinite',
+        },
+        keyframes: {
+          wiggle: {
+            '0%, 100%': { transform: 'rotate(-3deg)' },
+            '50%': { transform: 'rotate(3deg)' },
+          },
+        },
+      },
+    },
+    plugins: [],
+  }
+  
